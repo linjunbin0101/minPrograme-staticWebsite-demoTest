d2dbf222a3b9795583cfbf7b378e1328febf5519
linjunbin0101 <2879529995@qq.com>
2023-03-21 01:00:52 +0800

    add

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-6ginha5t91515134","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Soft\wxDevTool\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-6ginha5t91515134 --n quickstartFunctions --r --project "C:\Users\coffee\Desktop\xcx" --report_first --report
